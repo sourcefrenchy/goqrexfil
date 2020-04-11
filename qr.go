ffbb023cb45e9c6ae1610c52bdf1f83489bee6b1
JMA <1182482+sourcefrenchy@users.noreply.github.com>
2020-04-10 22:33:19 -0400

    Update qr.go
    Added minimal code to be able to encode stdin entry into a base64 payload and generate and display the related qr code

--- qr.go
+++ qr.go
@@ -2,18 +2,63 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	b64 "encoding/base64"
 	"flag"
 	"fmt"
+	"image"
+	"image/png"
 	"log"
 	"os"
+	"os/exec"
+	"runtime"
+
+	qrcode "github.com/skip2/go-qrcode"
 )
 
-func encodepayload(payload string) {
+func generateqr(payload string) string {
+	var pic []byte
+	pic, _ = qrcode.Encode(payload, qrcode.Medium, 256)
+	img, _, err := image.Decode(bytes.NewReader(pic))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	out, _ := os.Create("./qr.png")
+
+	if err := png.Encode(out, img); err != nil {
+		out.Close()
+		log.Fatal(err)
+	}
+	return "qr.png"
+}
+
+func encodepayload(payload string) string {
 	fmt.Println("[*] encodepayload", payload)
 	sEnc := b64.StdEncoding.EncodeToString([]byte(payload))
 	fmt.Println(sEnc)
+	return sEnc
 }
+
+func openbrowser(filename string) {
+	var err error
+	fmt.Println("[*] Displaying picture")
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", filename).Start()
+	case "windows":
+		err = exec.Command("rundll32", "filename.dll,FileProtocolHandler", filename).Start()
+	case "darwin":
+		err = exec.Command("open", filename).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
 func main() {
 	fmt.Println("-= goqrexfil =-")
 	isServer := flag.Bool("server", false, "server mode")
@@ -28,7 +73,9 @@ func main() {
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
 			fmt.Println("[*] Client mode: ON")
-			encodepayload(scanner.Text())
+			encoded := encodepayload(scanner.Text())
+			png := generateqr(encoded)
+			openbrowser(png)
 		}
 
 		if err := scanner.Err(); err != nil {
